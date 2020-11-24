106f29fd0076aaa55529dd6635030053f035eb6a
Vitoria <vitoriahellen3004@gmail.com>
2020-11-23 22:38:42 -0300

    Maximo Divisor Comum

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace mdc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA; 
+            int valorB;
+            int mdc;
+            bool encerrar = true; 
+             
+
+            Console.WriteLine("Máximo Divisor Comum");
+            Console.Write("Digite o 1° número: "); 
+            valorA = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("Digite o 2° número: ");
+            valorB = Convert.ToInt32(Console.ReadLine()); 
+
+            while (encerrar)
+            {
+            if (valorA % valorB ==0)
+            
+            {
+                Console.WriteLine($"MDC (a, b) = {valorA}");  
+                encerrar = false;
+            }
+            else
+            {
+               mdc = valorA % valorB; 
+               valorA = valorB; 
+               valorB = mdc;  
+               
+            }
+            }
+ 
+        }
+    }
+}
